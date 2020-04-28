ab9cd6ee21436cc0a209e3b19d2c7233c8ea2507
dannaldahye-ydf <3089253736@qq.com>
2020-04-28 21:40:14 +0800

    新增

---
+++ js/pro.js
@@ -0,0 +1,18 @@
+//顶部导航下拉菜单
+$('#jx').mouseenter(function () {
+    $('.diqu').show()
+})
+$('.diqu').mouseleave(function () {
+    $('.diqu').hide()
+})
+
+$('#xl1').mouseenter(function () {
+    $('.my-jd').show()
+})
+$('.my-jd').mouseleave(function () {
+    $('.my-jd').hide()
+})
+//广告
+$('#ad-close').click(function () {
+    $('.ad').css("display", "none")
+})
