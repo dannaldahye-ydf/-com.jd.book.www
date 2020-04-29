e81497381e75300077b5b954b6e19b6b45e3b231
dannaldahye-ydf <3089253736@qq.com>
2020-04-29 12:22:43 +0800

    新增

--- js/pro.js
+++ js/pro.js
@@ -16,3 +16,21 @@ $('.my-jd').mouseleave(function () {
 $('#ad-close').click(function () {
     $('.ad').css("display", "none")
 })
+
+// 移入创建遮罩层
+$('.main-content-1-lists .content-list').hover(function(){
+    $(this).append(`<div class="box"></div>`)
+    $(".box").css({
+        "opacity":"0.2",
+         "width":"188px",
+         "height":" 275px",
+         "backgroundColor":"#fff",
+         "position":"absolute",
+         "top":0,
+         "left":0
+    })
+    $(this).find("p").css("color","red")
+},function(){
+    $(".box").remove()
+    $(this).find("p").css("color","#333")
+})
