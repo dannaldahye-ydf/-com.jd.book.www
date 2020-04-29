75b57329094c2b22452d9d76342a219bf053cf3c
dannaldahye-ydf <3089253736@qq.com>
2020-04-30 00:14:48 +0800

    新增

---
+++ js/knowledge.js
@@ -0,0 +1,22 @@
+//顶部导航下拉菜单
+$('#jx').mouseenter(function () {
+    $('.diqu').show()
+})
+$('.diqu').mouseleave(function () {
+    $('.diqu').hide()
+})
+
+$('#xl1').mouseenter(function () {
+    $('.my-jd').show()
+})
+$('.my-jd').mouseleave(function () {
+    $('.my-jd').hide()
+})
+// 图书分类下拉
+$('#xl2').mouseenter(function () {
+    $('.search-book-xiala').show()
+    // console.log(111)
+})
+$('.search-book-xiala').mouseleave(function () {
+    $('.search-book-xiala').hide()
+})
